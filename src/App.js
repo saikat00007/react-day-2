1581c742a265ca3ace42cb87bbde87e825f86a02
saikat00007 <dev.shoukatulislam@gmail.com>
2022-09-05 21:14:11 +0600

    not-done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import './blog'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-function App() {
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+// import Alert from 'react-bootstrap/Alert';
+
+
+const Saikat = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home"><h2>km Saikat</h2></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto w-100 justify-content-end">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">blog</Nav.Link>
+            <Nav.Link href="#link">review</Nav.Link>
+            <Nav.Link href="#link">footer</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
   );
 }
 
-export default App;
+
+export default Saikat;
---
+++ src/blog/blog.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../blog/blog.css'
+
+const sum = () => {
+    return (
+        <div className='ganja'>saikat</div>
+    );
+}
+export default sum;
